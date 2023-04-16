d06f13089b3f22c714aea3826db6a59efbce47eb
Mert Arıcan <mert-arican@outlook.com>
2023-04-16 16:05:29 +0300

    Initial Commit

---
+++ 19011622/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  19011622
+//
+//  Created by Mert Arıcan on 16.04.2023.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
